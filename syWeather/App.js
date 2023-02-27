1c91d93a02d8e409d4ed7064b2897d4cc79b3268
thdud <thgml1444@naver.com>
2023-02-28 00:33:02 +0900

    react-native 시작

---
+++ syWeather/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>React-Native, ddddHello??</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
+    color: "red",
+  },
+});
