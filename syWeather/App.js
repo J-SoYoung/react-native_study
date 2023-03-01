a63f4e8d0ca6238ab38850ee0ad25dbb381fea66
thdud <thgml1444@naver.com>
2023-03-01 18:04:44 +0900

    react-native의 api와 컴포넌트를 사용하라 - flex 스타일을 적용하라 - Location api를 적용하여 기기의 위치를 받는다 - mock API를 연결하여 데이터 출력을 하여 화면에 보여주고 - react-native 컴포넌트에 props를 추가하여 다양한 기능을 추가하였다 - expo에서 제공하는 icon을 사용하였다.

--- syWeather/App.js
+++ syWeather/App.js
@@ -1,11 +1,109 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  pagingEnabled,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const { width: SCREEN_WIDTH } = Dimensions.get("window");
+  // console.log(SCREEN_WIDTH);
+
+  const API_KEY = "63b3e6ef6feaf501a5e28655a7de500c";
+
+  const icons = {
+    Person: "account-heart-outline",
+    Phone: "cellphone",
+    Call: "phone-classic",
+  };
+
+  const [city, setCity] = useState("Loading---");
+  const [person, setPerson] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getPermission = async () => {
+    // api 내용 안에 granted(위치 확인 동의) 의 여부
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false); //유저 거절
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].region);
+
+    const response = await fetch("https://koreanjson.com/users");
+    const list = await response.json();
+    setPerson(list);
+    // console.log(list[0]);
+  };
+
+  useEffect(() => {
+    getPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>React-Native, ddddHello??</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {person.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="#0e4f3d" size="large" />
+          </View>
+        ) : (
+          person.map((p) => {
+            // console.log(p.phone.split('-')[0] === '010'? true : false)
+            return (
+              <View key={p.id} style={styles.day}>
+                <View style={styles.iconAlign}>
+                  <MaterialCommunityIcons
+                    name="account-heart-outline"
+                    size={38}
+                    color="black"
+                  />
+                  <Text>이름: {p.name}</Text>
+                </View>
+                <Text>email: {p.email}</Text>
+                <View style={styles.iconAlign}>
+                  <MaterialCommunityIcons
+                    name={
+                      p.phone.split("-")[0] === "010" ? icons.Phone : icons.Call
+                    }
+                    size={24}
+                    color="black"
+                  />
+                  <Text>phone: {p.phone}</Text>
+                </View>
+                <Text>
+                  city: {p.province}-{p.city}-{p.district}-{p.street}
+                </Text>
+              </View>
+            );
+          })
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,12 +111,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#0e4f3d",
   },
-  text: {
-    fontSize: 50,
-    color: "red",
+  city: {
+    // backgroundColor: "#FE7167",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 50,
+  },
+  cityName: {
+    marginTop: 50,
+    color: "white",
+    fontSize: 48,
+  },
+  weather: {
+    backgroundColor: "#EEDFBE",
+  },
+  day: {
+    width: 414,
+    // width: SCREEN_WIDTH,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  temp: {
+    marginTop: -100,
+    fontSize: 160,
+    color: "#0e4f3d",
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+    color: "#0e4f3d",
+  },
+  iconAlign: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
